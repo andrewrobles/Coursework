0d60d1937d224a6883fdc807978425ad6166307e
andrewrobles <156987928+andrewrobles@users.noreply.github.com>
2024-07-03 12:53:52 -0700

    Write generated secret key to env file

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,6 +1,12 @@
+import fs from "fs"
 import { Keypair } from "@solana/web3.js";
 
 const keypair = Keypair.generate();
+
+// Write the secret key to the .env file
+const secretKeyString = keypair.secretKey.toString();
+fs.writeFileSync(".env", `SECRET_KEY=[${secretKeyString}]\n`, { flag: "w" });
+
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
 console.log(`✅ Finished!`);
